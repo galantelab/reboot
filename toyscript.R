1c175de2b59d4324499b6ce643143b194c7a2d64
flpsantos <fsantos@mochsl.org.br>
2020-05-31 16:59:57 -0300

    Choice of number of total genes now robust

--- toyscript.R
+++ toyscript.R
@@ -9,12 +9,12 @@ suppressMessages(library("tidyverse"))
 setwd(getwd())
 
 ####Gene expression retrieval###
-                                                        #Modifiable options:
+#Modifiable options:
 query <- GDCquery(project="TCGA-GBM",                   #project-tissue option
-        data.category="Transcriptome Profiling",        #category
-        data.type="Gene Expression Quantification",     #type
-        experimental.strategy="RNA-Seq",                #experimental strategy
-        workflow.type="HTSeq - FPKM")                   #Workflow, given abundance units
+                  data.category="Transcriptome Profiling",        #category
+                  data.type="Gene Expression Quantification",     #type
+                  experimental.strategy="RNA-Seq",                #experimental strategy
+                  workflow.type="HTSeq - FPKM")                   #Workflow, given abundance units
 
 GDCdownload(query, method = "api")                      #directory structure: "path"/GDCdata/TCGA-GBM/harmonized/Transcriptome_Profiling/Gen$
 
@@ -24,46 +24,51 @@ GDCdownload(query, method = "api")                      #directory structure: "p
 exp_data <- GDCprepare(query)
 expmatrix <- SummarizedExperiment::assay(exp_data)
 expression <- t(expmatrix) %>%
-        as.data.frame() %>%
-        rownames_to_column(var="barcode")
+   as.data.frame() %>%
+   rownames_to_column(var="barcode")
 
 #Survival status and follow_up time retrieval (clinical data)#
 barcode <- exp_data$barcode
 status <- exp_data$vital_status
 follow_up <- exp_data$days_to_last_follow_up
 tmp_clin <- cbind(barcode,status,follow_up) %>%
-        as.data.frame()
+   as.data.frame()
 tmp_clin$barcode <- as.character(tmp_clin$barcode)
 
 ###Integrating expression and clinical data###
 pre_inp <- left_join(expression,tmp_clin, by="barcode") %>%
-        drop_na() %>%
-        filter(status!="Not Reported")
+   drop_na() %>%
+   filter(status!="Not Reported")
+
+#Purge duplicated IDs from expression data
+pre_inp$barcode <- substring(text = pre_inp$barcode, first=0, last=12)
+pre_inp$barcode <- gsub("-","\\.", as.character(pre_inp$barcode))
+dupfor<-duplicated(pre_inp$barcode)
+duprev<-duplicated(pre_inp$barcode, fromLast=T)
+dup <- dupfor + duprev
+barfinalclin <- !(dup)
+uniqindex <- which(barfinalclin)
+pre_inp <- pre_inp[uniqindex,]
 
 #Formatting and tossing genes#
 len <- length(colnames(pre_inp))
 aux1 <- pre_inp[,c(1,len-1,len)]
 
 #Ensure there are no columns (genes) with only expression value equals to 0
-include_cols <- c()
-for (col in 2:(ncol(pre_inp)-2))
-{
-  max_value <- max(pre_inp[[col]])
-  if (max_value >= 0.001)
-  {
-    include_cols <- append(x = include_cols, values = col)
-  }
-}
+pre_inp <- pre_inp[,2:dim(pre_inp)[2]]
+exclude <- which(apply(pre_inp,2,max) < 0.001)
+pre_inp <- pre_inp[,-exclude]
 
-tmp_pre_inp <- pre_inp[,c(1,include_cols,(ncol(pre_inp)-1),ncol(pre_inp))]
-len <- length(colnames(tmp_pre_inp))
+#Performin gene choices
+len <- length(colnames(pre_inp))
 set.seed(50)
 random <- sample.int((len-2),50)        #Modify the number of genes as you wish (50).
-aux2 <- tmp_pre_inp[,random]
+aux2 <- pre_inp[,random]
 input1 <- cbind(aux1,aux2)
 colnames(input1) <- c("sample", "OS", "OS.time", colnames(input1[4:(dim(input1)[2])]))
 input1 <- input1 %>%
-        mutate(OS = if_else(OS=="Alive", 0, 1))
+   mutate(OS = if_else(OS=="Alive", 0, 1))
+
 
 #############################################
 
@@ -80,7 +85,7 @@ barcode <- gsub("-","\\.",barcode)
 IDH.status <- exp_data$paper_IDH.status
 MGMT.status <- exp_data$paper_MGMT.promoter.status
 first_clin <- cbind(barcode, IDH.status, MGMT.status) %>%
-		as.data.frame()
+   as.data.frame()
 
 #Edit and filter Drug info
 clinical.drug <- GDCprepare_clinic(query = query, clinical.info = "drug")
@@ -122,7 +127,7 @@ karnofsky_median <- median(new_clinical$karnofsky_performance_score, na.rm = T)
 new_clinical$karnofsky_performance_score <- ifelse(new_clinical$karnofsky_performance_score >= karnofsky_median, "HIGH", "LOW")
 
 for (col in colnames(new_clinical)){
-  new_clinical[[col]] <- droplevels(as.factor(new_clinical[[col]]))
+   new_clinical[[col]] <- droplevels(as.factor(new_clinical[[col]]))
 }
 
 #Merge all clinical tables
@@ -139,43 +144,34 @@ uniqindex <- which(barfinalclin)
 finalclin <- finalclin[uniqindex,]
 
 finalclin <- finalclin %>%
-  mutate(IDH.status = if_else(IDH.status=="1", "MUT", "WT")) %>%
-  mutate(MGMT.status = if_else(MGMT.status=="1", "Methylated", "Unmethylated"))
-
-#Purge duplicated IDs from expression data
-input1$sample <- substring(text = input1$sample, first=0, last=12)
-input1$sample <- gsub("-","\\.", as.character(input1$sample))
-dupfor<-duplicated(input1$sample)
-duprev<-duplicated(input1$sample, fromLast=T)
-dup <- dupfor + duprev
-barfinalclin <- !(dup)
-uniqindex <- which(barfinalclin)
-input1 <- slice(input1, uniqindex)
+   mutate(IDH.status = if_else(IDH.status=="1", "MUT", "WT")) %>%
+   mutate(MGMT.status = if_else(MGMT.status=="1", "Methylated", "Unmethylated"))
 
 #Get patients from clinical data only if there is corresponding expression data
-exp_patients <- as.vector(unique(as.character(input1$sample)))
+exp_patients <- as.factor(input1$sample)
+#exp_patients <- as.vector(unique(input1$sample))
 finalclin <- finalclin[finalclin$barcode %in% exp_patients,]
 
 for (col in colnames(finalclin)){
-  finalclin[[col]] <- droplevels(as.factor(finalclin[[col]]))
+   finalclin[[col]] <- droplevels(as.factor(finalclin[[col]]))
 }
 
 #Keep columns that have only 2 categories in clinical table
 keep_cols <- c(1)
 for (col in 2:ncol(finalclin))
 {
-  check <- nlevels(as_factor(finalclin[[col]]))
-  if (check == 2)
-  {
-    keep_cols <- append(x = keep_cols, values = col)
-  }
+   check <- nlevels(as_factor(finalclin[[col]]))
+   if (check == 2)
+   {
+      keep_cols <- append(x = keep_cols, values = col)
+   }
 }
 finalclin <- finalclin[,keep_cols]
 colnames(finalclin) <- c("barcode", "gender", "age", "race", "ethnicity","cancer.status",
                          "karnofsky","chemotherapy","IDH.status","MGMT.status")
 
 for (col in 2:ncol(finalclin)){
-  finalclin[[col]] <- tolower(finalclin[[col]])
+   finalclin[[col]] <- tolower(finalclin[[col]])
 }
 
 finalclin[is.na(finalclin)] <- "NA"
