d8377b3e4a4548d7eab69123671d8668272f0944
Filipe Ferreira dos Santos <fferreira@mochsl.org.br>
2026-04-30 19:05:39 -0300

    Update reboot.R
    Fix parser logical bug + "type" vs. "default" parameter values

--- reboot.R
+++ reboot.R
@@ -18,17 +18,17 @@ parser_reg$add_argument("-I", "--filein", type = "character", dest = "fname", me
                         help = "Input file name. Tab separated values (tsv) file containing expression and survival parameters")
 parser_reg$add_argument("-O", "--outprefix", type = "character", dest = "out", metavar = '', default = "reboot",
                         help = 'Output file prefix. Default: reboot')
-parser_reg$add_argument("-B","--bootstrap", type = "integer", dest = "boot_iter",  metavar = '', default = "1",
+parser_reg$add_argument("-B","--bootstrap", type = "integer", dest = "boot_iter",  metavar = '', default = 1,
                         help = "Number of iterations for bootstrap simulation (int). Default: 1")
-parser_reg$add_argument("-G", "--groupsize", type = "integer", dest = "nel",  metavar = '', default = "10",
+parser_reg$add_argument("-G", "--groupsize", type = "integer", dest = "nel",  metavar = '', default = 10,
                         help = "Number of genes/transcripts for each bootstrap simulation (int). Default: 10")
-parser_reg$add_argument("-P", "--pcentfilter", type = "numeric", dest = "pf",  metavar = '', default = "0.3",
-                        help = "Percentage of correlated feature pairs allowed in each iteration (numeric). Default: 0.3")
-parser_reg$add_argument("-T", "--type", type = "character", dest = "ty",  metavar = '', default = "gene",
+parser_reg$add_argument("-P", "--pcentfilter", type = "double", dest = "pf",  metavar = '', default = 0.3,
+                        help = "Percentage of correlated feature pairs allowed in each iteration (double). Default: 0.3")
+parser_reg$add_argument("-T", "--ty", type = "character", dest = "ty",  metavar = '', default = "gene",
                         help = 'Type of transcriptome data: gene or transcript (character). Default: gene')
-parser_reg$add_argument("-V", "--varfilter", type = "numeric", dest = "var",  metavar = '', default = "0.01",
-                        help = 'Minimum normalized variance (0-1) required for each feature among samples (numeric). Default: 0.01')
-parser_reg$add_argument("-F", "--force", type = "logical", dest = "force", action = "store_true", default = FALSE,
+parser_reg$add_argument("-V", "--varfilter", type = "double", dest = "var",  metavar = '', default = 0.01,
+                        help = 'Minimum normalized variance (0-1) required for each feature among samples (double). Default: 0.01')
+parser_reg$add_argument("-F", "--force", dest = "force", action = "store_true", default = FALSE,
                         help = "Choose -F to bypass OS and OS.time filters")
 #####################################################################################################################
 
@@ -41,42 +41,42 @@ parser_sur$add_argument("-O", "--outprefix", type = "character", dest = "out",  
                         help = 'Output file prefix. Default: reboot')
 parser_sur$add_argument("-S", "--signature", type = "character", dest = "sig",  metavar = '',
                         help = 'Tab separated values (tsv) file containing a set of features and corresponding regression coefficients')
-parser_sur$add_argument("-M", "--multivariate", type = "logical", dest = "type", action = "store_true", default = FALSE,
+parser_sur$add_argument("-M", "--multivariate", dest = "type", action = "store_true", default = FALSE,
                         help = 'If clinical variables should be included, choose -M. This option is tied with -C option')
 parser_sur$add_argument("-C", "--clinical", type = "character", dest = "clin_file",  metavar = '', default = "",
                         help = 'Tab separated values (tsv) file containing binary categorical variables only. Required if -M option is chosen')
-parser_sur$add_argument("-R", "--roc", type = "logical", dest = "roc_curve", action = "store_true", default = FALSE,
+parser_sur$add_argument("-R", "--roc", dest = "roc_curve", action = "store_true", default = FALSE,
                         help = 'If molecular score should be categorized according to a ROC curve instead of the median, choose -R')
-parser_sur$add_argument("-V", "--varfilter", type = "numeric", dest = "var",  metavar = '', default = "0.01",
-                        help = 'Minimum normalized variance (0-1) required for follow up time (numeric). Default: 0.01')
-parser_sur$add_argument("-F", "--force", type = "logical", dest = "force", action = "store_true", default = FALSE,
+parser_sur$add_argument("-V", "--varfilter", type = "double", dest = "var",  metavar = '', default = 0.01,
+                        help = 'Minimum normalized variance (0-1) required for follow up time (double). Default: 0.01')
+parser_sur$add_argument("-F", "--force", dest = "force", action = "store_true", default = FALSE,
                         help = "Choose -F to bypass OS and OS.time filters")
 #####################################################################################################################
 
 ########## create the parser for the "COMPLETE" command ##########
 parser_all <- subparsers$add_parser('complete', help = "generates and applies a molecular signature (integrated analysis)")
 
-parser_sur$add_argument("-I", "--filein", type = "character", dest = "fname",  metavar = '',
+parser_all$add_argument("-I", "--filein", type = "character", dest = "fname",  metavar = '',
                         help = "Input file name. Tab separated values (tsv) file containing expression and survival parameters")
-parser_sur$add_argument("-O", "--outprefix", type = "character", dest = "out",  metavar = '', default = "reboot",
+parser_all$add_argument("-O", "--outprefix", type = "character", dest = "out",  metavar = '', default = "reboot",
                         help = 'Output file prefix. Default: reboot')
-parser_sur$add_argument("-M", "--multivariate", type = "logical", dest = "type", action = "store_true", default = FALSE,
+parser_all$add_argument("-M", "--multivariate", dest = "type", action = "store_true", default = FALSE,
                         help = 'If clinical variables should be included, choose -M. This option is tied with -C option')
-parser_sur$add_argument("-C", "--clinical", type = "character", dest = "clin_file",  metavar = '', default = "",
+parser_all$add_argument("-C", "--clinical", type = "character", dest = "clin_file",  metavar = '', default = "",
                         help = 'Tab separated values (tsv) file containing binary categorical variables only. Required if -M option is chosen')
-parser_sur$add_argument("-R", "--roc", type = "logical", dest = "roc_curve", action = "store_true", default = FALSE,
+parser_all$add_argument("-R", "--roc", dest = "roc_curve", action = "store_true", default = FALSE,
                         help = 'If molecular score should be categorized according to a ROC curve instead of the median, choose -R')
-parser_reg$add_argument("-B","--bootstrap", type = "integer", dest = "boot_iter",  metavar = '', default = "1",
+parser_all$add_argument("-B","--bootstrap", type = "integer", dest = "boot_iter",  metavar = '', default = 1,
                         help = "Number of iterations for bootstrap simulation (int). Default: 1")
-parser_reg$add_argument("-G", "--groupsize", type = "integer", dest = "nel",  metavar = '', default = "10",
+parser_all$add_argument("-G", "--groupsize", type = "integer", dest = "nel",  metavar = '', default = 10,
                         help = "Number of genes/transcripts for each bootstrap simulation (int). Default: 10")
-parser_reg$add_argument("-P", "--pcentfilter", type = "numeric", dest = "pf",  metavar = '', default = "0.3",
-                        help = "Percentage of correlated feature pairs allowed in each iteration (numeric). Default: 0.3")
-parser_reg$add_argument("-T", "--type", type = "character", dest = "ty",  metavar = '', default = "gene",
+parser_all$add_argument("-P", "--pcentfilter", type = "double", dest = "pf",  metavar = '', default = 0.3,
+                        help = "Percentage of correlated feature pairs allowed in each iteration (double). Default: 0.3")
+parser_all$add_argument("-T", "--ty", type = "character", dest = "ty",  metavar = '', default = "gene",
                         help = 'Type of transcriptome data: gene or transcript (character). Default: gene')
-parser_reg$add_argument("-V", "--varfilter", type = "numeric", dest = "var",  metavar = '', default = "0.01",
-                        help = 'Minimum normalized variance (0-1) required for each feature and follow up time (numeric). Default: 0.01')
-parser_reg$add_argument("-F", "--force", type = "logical", dest = "force", action = "store_true", default = FALSE,
+parser_all$add_argument("-V", "--varfilter", type = "double", dest = "var",  metavar = '', default = 0.01,
+                        help = 'Minimum normalized variance (0-1) required for each feature and follow up time (double). Default: 0.01')
+parser_all$add_argument("-F", "--force", dest = "force", action = "store_true", default = FALSE,
                         help = "Choose -F to bypass OS and OS.time filters")
 #####################################################################################################################
 
@@ -96,7 +96,7 @@ if (args$sub_name == "regression") {
 		"-P", args$pf,
 		"-T", args$ty,
 		"-V", args$var,
-		"-F", args$force,
+		if (args$force) "-F",
 		collapse = " "))
 }
 ################################
@@ -117,11 +117,11 @@ if (args$sub_name == "survival") {
 			"-I", args$fname,
 			"-O", args$out,
 			"-S", args$sig,
-			"-M", args$type,
+			if (args$type) "-M",
 			"-C", args$clin_file,
-			"-R", args$roc_curve,
+			if (args$roc_curve) "-R",
 			"-V", args$var,
-			"-F", args$force,
+			if (args$force) "-F",
 			collapse = " "))
 		}
 	} else {
@@ -130,10 +130,10 @@ if (args$sub_name == "survival") {
 		"-I", args$fname,
 		"-O", args$out,
 		"-S", args$sig,
-		"-M", args$type,
-		"-R", args$roc_curve,
+		if (args$type) "-M",
+		if (args$roc_curve) "-R",
 		"-V", args$var,
-		"-F", args$force,
+		if (args$force) "-F",
 		collapse = " "))
 	}
 }
@@ -160,7 +160,7 @@ if (args$sub_name == "complete") {
 	"-P", args$pf,
 	"-T", args$ty,
 	"-V", args$var,
-	"-F", args$force,
+	if (args$force) "-F",
 	collapse = " "),
 	intern = TRUE)
 
@@ -179,11 +179,11 @@ if (args$sub_name == "complete") {
 			"-I", args$fname,
 			"-O", args$out,
 			"-S", int_sig,
-			"-M", args$type,
+			if (args$type) "-M",
 			"-C", args$clin_file,
-			"-R", args$roc_curve,
+			if (args$roc_curve) "-R",
 			"-V", args$var,
-			"-F", args$force,
+			if (args$force) "-F",
 			collapse = " "))
 		}
 	} else {
@@ -192,10 +192,10 @@ if (args$sub_name == "complete") {
 		"-I", args$fname,
 		"-O", args$out,
 		"-S", int_sig,
-		"-M", args$type,
-		"-R", args$roc_curve,
+		if (args$type) "-M",
+		if (args$roc_curve) "-R",
 		"-V", args$var,
-		"-F", args$force,
+		if (args$force) "-F",
 		collapse = " "))
 	}
 }
